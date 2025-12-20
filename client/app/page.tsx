876194a8b67204658e96e7a677db3bf4b7a2044e
Omkar Parhad <omkarparhad7905@gmail.com>
2025-12-20 18:09:48 +0530

    forn end setup

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { Button } from '@/components/ui/button'
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
-export default function App() {
+export default function Home() {
   return (
     <div>
       <Button>Button</Button>
     </div>
-  )
+  );
 }
